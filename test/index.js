c7823e525780a6677bd6fd702a5c0c7bfc9f7299
Ahmad Nassri <ahmad@codeinchaos.com>
2016-02-24 12:10:38 -0500

    feat(svg): add support for svg files

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ var imagemin = require('..')
 var async = require('async')
 
 describe('metalsmith-markdown', function () {
-  async.each(['gif', 'png', 'jpg'], function (ext) {
+  async.each(['gif', 'png', 'svg', 'jpg'], function (ext) {
     it('should minify ' + ext, function (done) {
       var file = 'test/fixtures/test.' + ext
       var files = {}
@@ -21,6 +21,20 @@ describe('metalsmith-markdown', function () {
 
         done()
       })
+    })
+  })
+
+  it('should not compress corrupted file', function (done) {
+    var file = 'test/fixtures/test-corrupt.jpg'
+    var files = {}
+    files[file] = { contents: fs.readFileSync(file) }
+
+    imagemin()(files, null, function () {
+      var original = fs.statSync(file).size
+
+      assert.ok(files[file].contents.length === original, file)
+
+      done()
     })
   })
 })
